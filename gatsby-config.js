bc738d9055aa5b1f860dc290f0b19428284a9fc4
Santi <santi.vdt@gmail.com>
2020-09-29 15:46:20 +0200

    new plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,58 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const path = require(`path`)
+const { slash } = require(`gatsby-core-utils`)
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  // query content for WordPress posts
+  const {
+    data: {
+      allWpPost: { nodes: allPosts },
+    },
+  } = await graphql(`
+    query {
+      allWpPost {
+        nodes {
+          id
+          uri
+        }
+      }
+    }
+  `)
+
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+
+  allPosts.forEach(post => {
+    createPage({
+      // will be the url for the page
+      path: post.uri,
+      // specify the component template of your choice
+      component: slash(postTemplate),
+      // In the ^template's GraphQL query, 'id' will be available
+      // as a GraphQL variable to query for this post's data.
+      context: {
+        id: post.id,
+      },
+    })
+  })
+}
 module.exports = {
     plugins: [
+      `gatsby-plugin-sharp`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-material-ui`,
+      {
+        resolve: `gatsby-plugin-sharp`
+      },
   {
     resolve: `gatsby-source-wordpress-experimental`,
     options: {
       url:
       // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
-        process.env.WPGRAPHQL_URL ||
+        'https://debovengrondse.nl/graphql' ||
         `https://localhost/graphql`,
       schema: {
         //Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
